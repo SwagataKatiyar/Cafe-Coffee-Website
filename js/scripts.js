4eef6949929dae75fbe1bbe0fa73fa028f19a2f9
renu katiyar <renu katiyar@DESKTOP-34H6K4F>
2021-10-10 18:24:57 +0530

    Add project files.

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+﻿
+window.addEventListener('DOMContentLoaded', event => {
+    const listHoursArray = document.body.querySelectorAll('.list-hours li');
+    listHoursArray[new Date().getDay()].classList.add(('today'));
+})
